0daad805a2731ff5e91329ff7c94ca2ef9a735e0
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-02 17:57:50 +0900

    memo/useCallbackを利用した再レンダリングの抑制

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState, useCallback } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  },[]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>PRINT</button>
+      <ChildArea visible={open} onClickClose={onClickClose}/>
     </div>
   );
-}
+};
